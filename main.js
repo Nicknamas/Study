744401bb4fa53070cac7b76562b18649143c8e14
imanuglyone <dr.xlson@yandex.ru>
2025-02-10 13:18:23 +0800

    add many pages with poems and authors this poems

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const button = document.getElementById("myButton")
+const backButton = document.getElementById("back")
 
-button.addEventListener("click", (event) => {
-    console.log("click")
-    console.debug("[EVENT]: ", event)
-    const newButton = document.createElement("button")
-    newButton.innerText = "New Button"
-    document.body.appendChild(newButton)
-    console.log("document contains new button: ", document.contains(newButton))
+backButton.addEventListener("click", () => {
+    history.back()
 })
